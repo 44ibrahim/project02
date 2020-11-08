cfed3befcd42cc98a261fa92f24a73ed281d8737
44ibrahim <72941719+44ibrahim@users.noreply.github.com>
2020-11-08 20:01:30 +0300

    forth commit from remote master branch

--- src/firstPackage/FirstClass.java
+++ src/firstPackage/FirstClass.java
@@ -9,6 +9,7 @@ public class FirstClass {
 		System.out.println("first commit from remote master branch");
 		System.out.println("second commit from remote master branch");
 		System.out.println("third commit from remote master branch");
+		System.out.println("forth commit from remote master branch");
 	}
 	
 
